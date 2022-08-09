4479883c508db28154034644c81743b85ca88077
martinabrle <martinabrle@users.noreply.github.com>
2022-08-09 18:24:01 +0200

    Testing MI

--- todo/src/main/java/app/demo/todo/DatabaseLoader.java
+++ todo/src/main/java/app/demo/todo/DatabaseLoader.java
@@ -6,7 +6,6 @@ import java.util.UUID;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
