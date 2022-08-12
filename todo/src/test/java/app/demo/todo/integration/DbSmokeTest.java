19d09c09d61ed57ffa21a9e5a0061fcaf71884ea
martinabrle <martinabrle@users.noreply.github.com>
2022-08-12 18:05:52 +0200

    Remove unnecessary import in DB Smoke Test

--- todo/src/test/java/app/demo/todo/integration/DbSmokeTest.java
+++ todo/src/test/java/app/demo/todo/integration/DbSmokeTest.java
@@ -1,13 +1,8 @@
 package app.demo.todo.integration;
 
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.condition.EnabledIfEnvironmentVariable;
-import org.junit.jupiter.api.condition.EnabledIfSystemProperty;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
-
-
 import app.demo.todo.repository.TodoRepository;
 
 @SpringBootTest
