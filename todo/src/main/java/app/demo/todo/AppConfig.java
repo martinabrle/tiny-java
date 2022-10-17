fb57ff01067a3f1cc43cc550f1e89e714c83a951
martinabrle <71389217+martinabrle@users.noreply.github.com>
2022-10-17 19:29:59 +0200

    Update AppConfig.java

--- todo/src/main/java/app/demo/todo/AppConfig.java
+++ todo/src/main/java/app/demo/todo/AppConfig.java
@@ -12,7 +12,6 @@ import com.azure.identity.ManagedIdentityCredentialBuilder;
 
 import app.demo.todo.utils.AppLogger;
 
-
 //https://www.azureblue.io/how-to-authenicated-aad-identity-against-postgres-using-spring-boot/
 @Configuration
 @ConfigurationProperties(prefix = "app.demo.todo")
